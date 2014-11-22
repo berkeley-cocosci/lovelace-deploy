2b94f00c123e6b1768e3458636ee7c233335f10b
Jessica B. Hamrick <jhamrick@berkeley.edu>
2014-11-21 17:09:09 -0800

    Remove compmodels stuff

--- roles/jupyterhub/files/make_userlist.sh
+++ roles/jupyterhub/files/make_userlist.sh
@@ -3,16 +3,16 @@
 # remove existing file
 rm -f $1
 
-# populate instructors and students
+# populate jupyterhub users
 users=$(cut -d: -f1 /etc/passwd)
 for user in $users; do
-    is_instructor=$(groups $user | grep "\\binstructors\\b")
-    if [[ ! -z "$is_instructor" ]]; then
+    is_jupyterhub_admin=$(groups $user | grep "\\bjupyterhub_admins\\b")
+    if [[ ! -z "$is_jupyterhub_admin" ]]; then
         echo "$user:$(id -u $user) admin" >> $1
     fi
 
-    is_student=$(groups $user | grep "\\bstudents\\b")
-    if [[ ! -z "$is_student" ]]; then
+    is_jupyterhub_user=$(groups $user | grep "\\bjupyterhub_users\\b")
+    if [[ ! -z "$is_jupyterhub_user" ]]; then
         echo "$user:$(id -u $user)" >> $1
     fi
 done
