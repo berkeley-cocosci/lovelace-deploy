c1240a0c6854c3fcaa706fe9d7986358bd6f8baf
Jessica B. Hamrick <jhamrick@berkeley.edu>
2016-12-10 12:11:51 +0100

    Fix bug in jupyterhub role

--- roles/jupyterhub/templates/jupyterhub_config.py
+++ roles/jupyterhub/templates/jupyterhub_config.py
@@ -5,8 +5,9 @@ c = get_config()
 c.JupyterHub.ip = "0.0.0.0"
 c.JupyterHub.port = 80
 c.JupyterHub.log_level = 10
-c.JupyterHub.pid_file = "{{ root }}/pid"
+c.JupyterHub.pid_file = "{{ jupyterhub_root }}/pid"
 c.JupyterHub.confirm_no_ssl = True
+c.JupyterHub.db_url = "sqlite://{{ jupyterhub_root }}/jupyterhub.sqlite"
 c.Authenticator.admin_users = admin = set()
 
 # Set the full path to the singleuser server
@@ -16,7 +17,7 @@ c.LocalProcessSpawner.cmd = ["/usr/local/bin/jupyterhub-singleuser"]
 c.Authenticator.whitelist = whitelist = set()
 
 # Add users to the admin list, the whitelist, and also record their user ids
-with open("{{ root }}/userlist") as f:
+with open("{{ jupyterhub_root }}/userlist") as f:
     for line in f:
         if not line:
             continue
